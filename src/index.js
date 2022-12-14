54c8689e4da3de65bb5b94ccd7587f2f7485ce0b
GIA CUONG <tgiacuong@gmail.com>
2022-12-14 23:07:39 +0900

    fix req.body

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const port = 3000
 const route = require('./routes')
 const db = require('./config/db')
 
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 // Connect to db
 db.connect();
 
