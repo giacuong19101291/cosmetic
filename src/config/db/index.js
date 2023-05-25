23b8fee8438168c51f1c96438191ba4eed302e20
GIA CUONG <tgiacuong@gmail.com>
2023-05-26 07:43:32 +0900

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-
     try {
-        await mongoose.connect('mongodb://localhost:27017/comestic_web', {
+        await mongoose.connect('mongodb+srv://giacuong:Zingspeed157%23@atlascluster.fik4okt.mongodb.net/comestic_web', {
+        // await mongoose.connect('mongodb://localhost:27017/comestic_web', {
             useNewUrlParser: true,
             useUnifiedtopology: true
         });
